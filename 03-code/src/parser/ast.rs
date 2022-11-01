65c61c2b1fe50915dfb1d9eb98f8af247c57303d
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2022-11-01 14:41:58 +0000

    more parser

--- 03-code/src/parser/ast.rs
+++ 03-code/src/parser/ast.rs
@@ -22,6 +22,8 @@ pub enum Statement {
   Switch(Box<Expression>, Box<Statement>),
   Labelled(LabelledStatement),
   Expr(Box<Expression>),
+  Declaration(Vec<SpecifierQualifier>, Vec<DeclaratorInitialiser>),
+  FunctionDeclaration(Vec<SpecifierQualifier>, Box<Declarator>, Box<Statement>),
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -32,7 +34,7 @@ pub enum LabelledStatement {
 }
 
 #[derive(Debug)]
-pub struct StatementList(pub Vec<Statement>);
+pub struct StatementList(pub Vec<Box<Statement>>);
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Expression {
@@ -49,11 +51,11 @@ pub enum Expression {
   PrefixDecrement(Box<Expression>),
   UnaryOp(UnaryOperator, Box<Expression>),
   SizeOfExpr(Box<Expression>),
-  SizeOfType(TypeSpecifier),
+  SizeOfType(TypeName),
   BinaryOp(BinaryOperator, Box<Expression>, Box<Expression>),
   Ternary(Box<Expression>, Box<Expression>, Box<Expression>),
   Assignment(Box<Expression>, Box<Expression>, Option<BinaryOperator>),
-  Cast(TypeSpecifier, Box<Expression>),
+  Cast(TypeName, Box<Expression>),
   ExpressionList(Box<Expression>, Box<Expression>),
 }
 
@@ -91,12 +93,31 @@ pub enum BinaryOperator {
 }
 
 #[derive(Debug, Clone, PartialEq)]
+pub enum SpecifierQualifier {
+  TypeSpecifier(TypeSpecifier),
+  StorageClassSpecifier(StorageClassSpecifier),
+  TypeQualifier(TypeQualifier),
+}
+
+#[derive(Debug, Clone, PartialEq)]
 pub enum TypeSpecifier {
-  ArithmeticType(ArithmeticType),
+  ArithmeticType(ArithmeticTypeSpecifier),
   Void,
   Struct(StructType),
   Union(UnionType),
   Enum(Option<Identifier>, Vec<Enumerator>),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum ArithmeticTypeSpecifier {
+  Char,
+  Short,
+  Int,
+  Long,
+  Signed,
+  Unsigned,
+  Float,
+  Double,
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -116,8 +137,11 @@ pub enum ArithmeticType {
 #[derive(Debug, Clone, PartialEq)]
 pub enum StructType {
   Declaration(Identifier),
-  Definition(Option<Identifier>),
+  Definition(Option<Identifier>, Vec<StructMemberDeclaration>),
 }
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct StructMemberDeclaration(pub Vec<SpecifierQualifier>, pub Vec<Box<Declarator>>);
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum UnionType {
@@ -130,3 +154,55 @@ pub enum Enumerator {
   Simple(Identifier),
   WithValue(Identifier, Box<Expression>),
 }
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum StorageClassSpecifier {
+  Auto,
+  Extern,
+  Register,
+  Static,
+  Typedef,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum TypeQualifier {
+  Const,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Declarator {
+  Identifier(Identifier),
+  PointerDeclarator(Box<Declarator>),
+  AbstractPointerDeclarator,
+  ArrayDeclarator(Box<Declarator>, Option<Box<Expression>>),
+  FunctionDeclarator(Box<Declarator>, Option<ParameterTypeList>),
+}
+
+// #[derive(Debug, Clone, PartialEq)]
+// pub enum AbstractDeclarator {
+//   PointerDeclarator,
+//   ArrayDeclarator(Box<AbstractDeclarator>, Option<Box<Expression>>),
+//   FunctionDeclarator(Box<AbstractDeclarator>, Option<ParameterTypeList>),
+// }
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum ParameterTypeList {
+  Normal(Vec<ParameterDeclaration>),
+  Variadic(Vec<ParameterDeclaration>),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum ParameterDeclaration {
+  Named(Vec<SpecifierQualifier>, Box<Declarator>),
+  // Abstract(Vec<SpecifierQualifier>, Option<Box<AbstractDeclarator>>),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum DeclaratorInitialiser {
+  NoInit(Box<Declarator>),
+  Init(Box<Declarator>, Box<Expression>),
+  Function(Box<Declarator>, Box<Statement>),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct TypeName(pub Vec<SpecifierQualifier>, pub Option<Box<Declarator>>);
