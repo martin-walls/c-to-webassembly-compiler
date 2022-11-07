49247167cb4c3474947ed180e4a7d18dca55a191
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2022-11-07 18:31:11 +0000

    stdbool header file

---
+++ 03-code/headers/bool.h
@@ -0,0 +1,11 @@
+#ifndef _STDBOOL_H
+#define _STDBOOL_H
+
+#define bool        _Bool
+#define true        1
+#define false       0
+
+/* Signal that all the definitions are present.  */
+#define __bool_true_false_are_defined        1
+
+#endif
