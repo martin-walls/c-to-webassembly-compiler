3acaf0847b0d4df384e3a6aa9529986b3f1f602a
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2022-11-10 17:09:52 +0000

    download some single-file C source programs for testing

---
+++ 02-test-programs/12-single-source-programs/case.c
@@ -0,0 +1,54 @@
+//
+// case.c
+//
+// Copyright (c) 2014 Stephen Mathieson
+// MIT licensed
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "case.h"
+
+#define CASE_IS_SEP(c)    ((c) == '-' || (c) == '_' || (c) == ' ')
+
+char *
+case_upper(char *str) {
+    for (char *s = str; *s; s++) {
+        *s = toupper(*s);
+    }
+    return str;
+}
+
+char *
+case_lower(char *str) {
+    for (char *s = str; *s; s++) {
+        *s = tolower(*s);
+    }
+    return str;
+}
+
+char *
+case_camel(char *str) {
+    char *r = str, *w = str;
+    // never cap the first char
+    while (CASE_IS_SEP(*r)) {
+        r++;
+    }
+    while (*r && !CASE_IS_SEP(*r)) {
+        *w++ = *r++;
+    }
+    while (*r) {
+        do {
+            r++;
+        } while (CASE_IS_SEP(*r));
+        *w++ = toupper(*r);
+        r++;
+        while (*r && !CASE_IS_SEP(*r)) {
+            *w++ = *r++;
+        }
+    }
+    *w = 0;
+    return str;
+}
