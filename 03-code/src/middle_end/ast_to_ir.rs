27088f5db37edd3c6d79508ec40809f1acb19e20
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2022-11-25 15:51:26 +0000

    refactor get_conversion_instrs function out of Instruction

--- 03-code/src/middle_end/ast_to_ir.rs
+++ 03-code/src/middle_end/ast_to_ir.rs
@@ -1334,7 +1334,7 @@ fn convert_expression_to_ir(
             // get conversion instrs into cast type
             let expr_var_type = expr_var.get_type(prog)?;
             let (mut cast_instrs, dest) =
-                Instruction::get_conversion_instrs(expr_var, expr_var_type, cast_type, prog)?;
+                get_type_conversion_instrs(expr_var, expr_var_type, cast_type, prog)?;
             instrs.append(&mut cast_instrs);
             Ok((instrs, dest))
         }
@@ -1543,12 +1543,8 @@ fn unary_convert(
     let src_type = src.get_type(prog)?;
     let unary_converted_type = src_type.unary_convert();
     if src_type != unary_converted_type {
-        let (instrs, converted_var) = Instruction::get_conversion_instrs(
-            src,
-            src_type,
-            unary_converted_type.to_owned(),
-            prog,
-        )?;
+        let (instrs, converted_var) =
+            get_type_conversion_instrs(src, src_type, unary_converted_type.to_owned(), prog)?;
         return Ok((instrs, converted_var));
     }
     Ok((Vec::new(), src))
@@ -1704,8 +1700,229 @@ fn convert_type_for_assignment(
 ) -> Result<(Vec<Instruction>, Src), MiddleEndError> {
     if src_type.is_arithmetic_type() && dest_type.is_arithmetic_type() {
         let (convert_instrs, converted_var) =
-            Instruction::get_conversion_instrs(src, src_type, dest_type, prog)?;
+            get_type_conversion_instrs(src, src_type, dest_type, prog)?;
         return Ok((convert_instrs, converted_var));
     }
     todo!("other options of possible type combinations")
+}
+
+fn get_type_conversion_instrs(
+    src: Src,
+    src_type: Box<IrType>,
+    dest_type: Box<IrType>,
+    prog: &mut Box<Program>,
+) -> Result<(Vec<Instruction>, Src), MiddleEndError> {
+    println!("convert {} to {}", src_type, dest_type);
+    let mut instrs = Vec::new();
+    if src_type == dest_type {
+        return Ok((instrs, src));
+    }
+    match (*src_type, *dest_type) {
+        // char promotions
+        (IrType::I8, dest_type) => {
+            let intermediate_var = prog.new_var(src.get_value_type());
+            let mut intermediate_type;
+            if dest_type.is_signed_integral() {
+                instrs.push(Instruction::I8toI16(intermediate_var.to_owned(), src));
+                intermediate_type = IrType::I16;
+            } else {
+                // unsigned
+                instrs.push(Instruction::I8toU16(intermediate_var.to_owned(), src));
+                intermediate_type = IrType::U16;
+            }
+            prog.add_var_type(
+                intermediate_var.to_owned(),
+                Box::new(intermediate_type.to_owned()),
+            )?;
+            let (mut convert_instrs, dest) = get_type_conversion_instrs(
+                Src::Var(intermediate_var),
+                Box::new(intermediate_type),
+                Box::new(dest_type),
+                prog,
+            )?;
+            instrs.append(&mut convert_instrs);
+            Ok((instrs, dest))
+        }
+        (IrType::U8, dest_type) => {
+            let intermediate_var = prog.new_var(src.get_value_type());
+            let mut intermediate_type;
+            if dest_type.is_signed_integral() {
+                instrs.push(Instruction::U8toI16(intermediate_var.to_owned(), src));
+                intermediate_type = IrType::I16;
+            } else {
+                // unsigned
+                instrs.push(Instruction::U8toU16(intermediate_var.to_owned(), src));
+                intermediate_type = IrType::U16;
+            }
+            prog.add_var_type(
+                intermediate_var.to_owned(),
+                Box::new(intermediate_type.to_owned()),
+            )?;
+            let (mut convert_instrs, dest) = get_type_conversion_instrs(
+                Src::Var(intermediate_var),
+                Box::new(intermediate_type),
+                Box::new(dest_type),
+                prog,
+            )?;
+            instrs.append(&mut convert_instrs);
+            Ok((instrs, dest))
+        }
+        (IrType::I16, dest_type) => {
+            let intermediate_var = prog.new_var(src.get_value_type());
+            let mut intermediate_type;
+            if dest_type.is_signed_integral() {
+                instrs.push(Instruction::I16toI32(intermediate_var.to_owned(), src));
+                intermediate_type = IrType::I32;
+            } else {
+                // unsigned
+                instrs.push(Instruction::I16toU32(intermediate_var.to_owned(), src));
+                intermediate_type = IrType::U32;
+            }
+            prog.add_var_type(
+                intermediate_var.to_owned(),
+                Box::new(intermediate_type.to_owned()),
+            )?;
+            let (mut convert_instrs, dest) = get_type_conversion_instrs(
+                Src::Var(intermediate_var),
+                Box::new(intermediate_type),
+                Box::new(dest_type),
+                prog,
+            )?;
+            instrs.append(&mut convert_instrs);
+            Ok((instrs, dest))
+        }
+        (IrType::U16, dest_type) => {
+            let intermediate_var = prog.new_var(src.get_value_type());
+            let mut intermediate_type;
+            if dest_type.is_signed_integral() {
+                instrs.push(Instruction::U16toI32(intermediate_var.to_owned(), src));
+                intermediate_type = IrType::I32;
+            } else {
+                // unsigned
+                instrs.push(Instruction::U16toU32(intermediate_var.to_owned(), src));
+                intermediate_type = IrType::U32;
+            }
+            prog.add_var_type(
+                intermediate_var.to_owned(),
+                Box::new(intermediate_type.to_owned()),
+            )?;
+            let (mut convert_instrs, dest) = get_type_conversion_instrs(
+                Src::Var(intermediate_var),
+                Box::new(intermediate_type),
+                Box::new(dest_type),
+                prog,
+            )?;
+            instrs.append(&mut convert_instrs);
+            Ok((instrs, dest))
+        }
+        (IrType::I32, IrType::U32) => {
+            let dest = prog.new_var(src.get_value_type());
+            prog.add_var_type(dest.to_owned(), Box::new(IrType::U32))?;
+            instrs.push(Instruction::I32toU32(dest.to_owned(), src));
+            Ok((instrs, Src::Var(dest)))
+        }
+        (IrType::I32, IrType::I64) => {
+            let dest = prog.new_var(src.get_value_type());
+            prog.add_var_type(dest.to_owned(), Box::new(IrType::I64))?;
+            instrs.push(Instruction::I32toI64(dest.to_owned(), src));
+            Ok((instrs, Src::Var(dest)))
+        }
+        (IrType::I32, IrType::U64) => {
+            let dest = prog.new_var(src.get_value_type());
+            prog.add_var_type(dest.to_owned(), Box::new(IrType::U64))?;
+            instrs.push(Instruction::I32toU64(dest.to_owned(), src));
+            Ok((instrs, Src::Var(dest)))
+        }
+        (IrType::I32, IrType::F32) => {
+            let dest = prog.new_var(src.get_value_type());
+            prog.add_var_type(dest.to_owned(), Box::new(IrType::F32))?;
+            instrs.push(Instruction::I32toF32(dest.to_owned(), src));
+            Ok((instrs, Src::Var(dest)))
+        }
+        (IrType::I32, IrType::F64) => {
+            let dest = prog.new_var(src.get_value_type());
+            prog.add_var_type(dest.to_owned(), Box::new(IrType::F64))?;
+            instrs.push(Instruction::I32toF64(dest.to_owned(), src));
+            Ok((instrs, Src::Var(dest)))
+        }
+
+        (IrType::U32, IrType::I64) => {
+            let dest = prog.new_var(src.get_value_type());
+            prog.add_var_type(dest.to_owned(), Box::new(IrType::I64))?;
+            instrs.push(Instruction::U32toI64(dest.to_owned(), src));
+            Ok((instrs, Src::Var(dest)))
+        }
+        (IrType::U32, IrType::U64) => {
+            let dest = prog.new_var(src.get_value_type());
+            prog.add_var_type(dest.to_owned(), Box::new(IrType::U64))?;
+            instrs.push(Instruction::U32toU64(dest.to_owned(), src));
+            Ok((instrs, Src::Var(dest)))
+        }
+        (IrType::U32, IrType::F32) => {
+            let dest = prog.new_var(src.get_value_type());
+            prog.add_var_type(dest.to_owned(), Box::new(IrType::F32))?;
+            instrs.push(Instruction::U32toF32(dest.to_owned(), src));
+            Ok((instrs, Src::Var(dest)))
+        }
+        (IrType::U32, IrType::F64) => {
+            let dest = prog.new_var(src.get_value_type());
+            prog.add_var_type(dest.to_owned(), Box::new(IrType::F64))?;
+            instrs.push(Instruction::U32toF64(dest.to_owned(), src));
+            Ok((instrs, Src::Var(dest)))
+        }
+
+        (IrType::I64, IrType::U64) => {
+            let dest = prog.new_var(src.get_value_type());
+            prog.add_var_type(dest.to_owned(), Box::new(IrType::U64))?;
+            instrs.push(Instruction::I64toU64(dest.to_owned(), src));
+            Ok((instrs, Src::Var(dest)))
+        }
+        (IrType::I64, IrType::F32) => {
+            let dest = prog.new_var(src.get_value_type());
+            prog.add_var_type(dest.to_owned(), Box::new(IrType::F32))?;
+            instrs.push(Instruction::I64toF32(dest.to_owned(), src));
+            Ok((instrs, Src::Var(dest)))
+        }
+        (IrType::I64, IrType::F64) => {
+            let dest = prog.new_var(src.get_value_type());
+            prog.add_var_type(dest.to_owned(), Box::new(IrType::F64))?;
+            instrs.push(Instruction::I64toF64(dest.to_owned(), src));
+            Ok((instrs, Src::Var(dest)))
+        }
+
+        (IrType::U64, IrType::F32) => {
+            let dest = prog.new_var(src.get_value_type());
+            prog.add_var_type(dest.to_owned(), Box::new(IrType::F32))?;
+            instrs.push(Instruction::U64toF32(dest.to_owned(), src));
+            Ok((instrs, Src::Var(dest)))
+        }
+        (IrType::U64, IrType::F64) => {
+            let dest = prog.new_var(src.get_value_type());
+            prog.add_var_type(dest.to_owned(), Box::new(IrType::F64))?;
+            instrs.push(Instruction::U64toF64(dest.to_owned(), src));
+            Ok((instrs, Src::Var(dest)))
+        }
+
+        (IrType::F32, IrType::F64) => {
+            let dest = prog.new_var(src.get_value_type());
+            prog.add_var_type(dest.to_owned(), Box::new(IrType::F64))?;
+            instrs.push(Instruction::F32toF64(dest.to_owned(), src));
+            Ok((instrs, Src::Var(dest)))
+        }
+
+        (IrType::Function(_, _), IrType::PointerTo(t))
+        | (IrType::ArrayOf(_, _), IrType::PointerTo(t)) => {
+            let dest = prog.new_var(src.get_value_type());
+            prog.add_var_type(dest.to_owned(), Box::new(IrType::PointerTo(t)))?;
+            instrs.push(Instruction::AddressOf(dest.to_owned(), src));
+            Ok((instrs, Src::Var(dest)))
+        }
+        (s, d) => {
+            return Err(MiddleEndError::TypeError(TypeError::TypeConversionError(
+                "Cannot convert type",
+                Box::new(s),
+                Some(Box::new(d)),
+            )))
+        } // todo rest of types
+    }
 }
