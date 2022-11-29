b00f81710e6b0d44461173e4cc84f7b470edea67
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2022-11-29 16:22:47 +0000

    remove Nops as well

--- 03-code/src/middle_end/middle_end_optimiser/remove_redundancy.rs
+++ 03-code/src/middle_end/middle_end_optimiser/remove_redundancy.rs
@@ -3,6 +3,10 @@ use crate::middle_end::instructions::Instruction;
 use crate::middle_end::middle_end_error::MiddleEndError;
 use std::collections::HashMap;
 
+/// Check through the instructions and remove any labels that aren't the target of
+/// a branch instruction.
+///
+/// Also remove any Nop instructions while we're here.
 pub fn remove_unused_labels(instrs: &mut Vec<Instruction>) -> Result<(), MiddleEndError> {
     // map of label: used
     let mut labels: HashMap<LabelId, bool> = HashMap::new();
@@ -32,31 +36,16 @@ pub fn remove_unused_labels(instrs: &mut Vec<Instruction>) -> Result<(), MiddleE
 
     // remove all instructions for which the closure returns false
     instrs.retain(|instr| {
-        if let Instruction::Label(label_id) = instr {
-            // if label is unused, remove the instruction
-            return labels.get(&label_id).unwrap().to_owned();
+        match instr {
+            Instruction::Label(label_id) => {
+                // if label is unused, remove the instruction
+                return labels.get(&label_id).unwrap().to_owned();
+            }
+            // also remove nops
+            Instruction::Nop => false,
+            _ => true,
         }
-        true
     });
-
-    // let mut instrs_to_remove = Vec::new();
-
-    // for instr_i in 0..instrs.len() {
-    //     if let Instruction::Label(label_id) = instrs.get(instr_i).unwrap() {
-    //         // we can safely unwrap because we just iterated and found all the labels
-    //         // in the last step
-    //         if !labels.get(label_id).unwrap() {
-    //             // if label is unused, remove the label instruction
-    //             // don't do the removing inside the loop, cos it'll mess up the
-    //             // loop index
-    //             instrs_to_remove.push(instr_i);
-    //         }
-    //     }
-    // }
-    //
-    // for instr_i in instrs_to_remove {
-    //     instrs.remove(instr_i);
-    // }
 
     Ok(())
 }
