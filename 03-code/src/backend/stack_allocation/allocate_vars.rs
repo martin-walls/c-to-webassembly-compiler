3f9b9131a0b5bdd9e8c7228077204eaf12c9fef4
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2023-02-03 13:57:27 +0000

    move shared allocation (params, return) to parent method

--- 03-code/src/backend/stack_allocation/allocate_vars.rs
+++ 03-code/src/backend/stack_allocation/allocate_vars.rs
@@ -1,4 +1,5 @@
 use crate::backend::memory_constants::PTR_SIZE;
+use crate::backend::stack_allocation::get_vars_from_block::get_vars_from_block;
 use crate::backend::stack_allocation::naive_allocation::{
     naive_allocate_global_vars, naive_allocate_local_vars,
 };
@@ -25,21 +26,61 @@ pub fn allocate_local_vars(
     prog_metadata: &Box<ProgramMetadata>,
     enabled_optimisations: &EnabledOptimisations,
 ) -> VariableAllocationMap {
+    let mut var_offsets: VariableAllocationMap = HashMap::new();
+    let mut offset = PTR_SIZE;
+
+    let (return_type, param_types) = match *fun_type {
+        IrType::Function(return_type, param_types, _is_variadic) => (return_type, param_types),
+        _ => unreachable!(),
+    };
+
+    // increment offset by return value size
+    let return_type_byte_size = match return_type.get_byte_size(prog_metadata) {
+        TypeSize::CompileTime(size) => size,
+        TypeSize::Runtime(_) => {
+            unreachable!()
+        }
+    };
+    offset += return_type_byte_size as u32;
+
+    // calculate offset of each param variable
+    for param_i in 0..param_types.len() {
+        let param_type = param_types.get(param_i).unwrap();
+        let param_var_id = fun_param_var_mappings.get(param_i).unwrap();
+        let param_byte_size = match param_type.get_byte_size(prog_metadata) {
+            TypeSize::CompileTime(size) => size,
+            TypeSize::Runtime(_) => {
+                unreachable!()
+            }
+        };
+        var_offsets.insert(param_var_id.to_owned(), offset);
+        offset += param_byte_size as u32;
+    }
+
+    // get all vars used in this block -- all the variables to allocate
+    let mut local_vars = get_vars_from_block(block, prog_metadata);
+    // remove param vars, cos we don't need to allocate them again
+    for param_var in &fun_param_var_mappings {
+        local_vars.remove(param_var);
+    }
+
     if enabled_optimisations.is_stack_allocation_optimisation_enabled() {
         optimised_allocate_local_vars(
+            local_vars,
             block,
+            offset,
+            var_offsets,
             wasm_instrs,
-            fun_type,
             fun_param_var_mappings,
             module_context,
             prog_metadata,
         )
     } else {
         naive_allocate_local_vars(
-            block,
+            local_vars,
+            offset,
+            var_offsets,
             wasm_instrs,
-            fun_type,
-            fun_param_var_mappings,
             module_context,
             prog_metadata,
         )
--- 03-code/src/backend/stack_allocation/naive_allocation.rs
+++ 03-code/src/backend/stack_allocation/naive_allocation.rs
@@ -11,58 +11,17 @@ use crate::relooper::blocks::Block;
 use std::collections::{HashMap, HashSet};
 
 pub fn naive_allocate_local_vars(
-    block: &Box<Block>,
+    vars: HashMap<VarId, Box<IrType>>,
+    start_offset: u32,
+    mut var_offsets: VariableAllocationMap,
     wasm_instrs: &mut Vec<WasmInstruction>,
-    fun_type: Box<IrType>,
-    fun_param_var_mappings: Vec<VarId>,
     module_context: &ModuleContext,
     prog_metadata: &Box<ProgramMetadata>,
 ) -> VariableAllocationMap {
-    let block_vars = get_vars_from_block(block, prog_metadata);
-
-    let mut var_offsets = HashMap::new();
-    let mut offset = PTR_SIZE;
-    // only increment the stack ptr by the size we allocated for local vars, not params and return value
-    let mut stack_ptr_increment = 0;
-
-    let (return_type, param_types) = match *fun_type {
-        IrType::Function(return_type, param_types, _is_variadic) => (return_type, param_types),
-        _ => unreachable!(),
-    };
-
-    // increment offset by return value size
-    let return_type_byte_size = match return_type.get_byte_size(prog_metadata) {
-        TypeSize::CompileTime(size) => size,
-        TypeSize::Runtime(_) => {
-            unreachable!()
-        }
-    };
-    offset += return_type_byte_size as u32;
-
-    let mut param_var_ids = HashSet::new();
-
-    // calculate offset of each param variable
-    for param_i in 0..param_types.len() {
-        let param_type = param_types.get(param_i).unwrap();
-        let param_var_id = fun_param_var_mappings.get(param_i).unwrap();
-        param_var_ids.insert(param_var_id.to_owned());
-        let param_byte_size = match param_type.get_byte_size(prog_metadata) {
-            TypeSize::CompileTime(size) => size,
-            TypeSize::Runtime(_) => {
-                unreachable!()
-            }
-        };
-        var_offsets.insert(param_var_id.to_owned(), offset);
-        offset += param_byte_size as u32;
-    }
+    let mut offset = 0;
 
     // calculate offset of each local variable
-    for (var_id, var_type) in block_vars {
-        // check if the variable is also a parameter and has therefore
-        // already been allocated
-        if param_var_ids.contains(&var_id) {
-            continue;
-        }
+    for (var_id, var_type) in vars {
         let byte_size = match var_type.get_byte_size(prog_metadata) {
             TypeSize::CompileTime(byte_size) => byte_size,
             TypeSize::Runtime(_) => {
@@ -72,13 +31,12 @@ pub fn naive_allocate_local_vars(
                 unreachable!()
             }
         };
-        var_offsets.insert(var_id, offset);
+        var_offsets.insert(var_id, start_offset + offset);
         offset += byte_size as u32;
-        stack_ptr_increment += byte_size as u32;
     }
 
     // update stack pointer to after allocated vars
-    increment_stack_ptr_by_known_offset(stack_ptr_increment, wasm_instrs, module_context);
+    increment_stack_ptr_by_known_offset(offset, wasm_instrs, module_context);
 
     var_offsets
 }
