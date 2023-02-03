e7b72e497a01e8da82e8ba08b28ffe6bd8ebe18c
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2023-02-03 09:20:14 +0000

    stack allocation tests

---
+++ 02-test-programs/16-stack-allocation/00-stack-allocation-test-1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    int x = 3;
+    long y = 100;
+    long z = x + y;
+    printf("y = %ld, z = %ld", y, z);
+    return 0;
+}
---
+++ 02-test-programs/16-stack-allocation/01-stack-allocation-test-2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    int arr[5];
+    int x = 6;
+    long y = 200;
+    arr[0] = x;
+    arr[1] = (int) y;
+    char c = 'a';
+    char d = 'f';
+    return c > d;
+}
