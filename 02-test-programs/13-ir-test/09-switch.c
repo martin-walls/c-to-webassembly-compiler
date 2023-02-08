e1a5db92b73bcabe4117d3ef2f823be243dc0c8e
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2023-02-08 15:20:51 +0000

    ✅ update switch test

--- 02-test-programs/13-ir-test/09-switch.c
+++ 02-test-programs/13-ir-test/09-switch.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-    int x = 1;
     for (int x = 1; x < 7; x++) {
         switch (x) {
             case 1:
             case 2:
-                // printf("less than 3\n");
+                printf("less than 3\n");
                 break;
             case 3:
-                //     printf("equal to 3\n");
+                printf("equal to 3\n");
                 break;
             case 4:
-                // printf("equal to 4\n");
+                printf("equal to 4\n");
             default:
             case 6:
-                // printf("greater than 3\n");
+                printf("greater than 3\n");
                 break;
         }
     }
