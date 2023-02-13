0b9338511b23fb90e36f8fe35e5e4dd8f4668d57
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2023-02-13 23:50:00 +0000

    🔨 only run commit hook tests if code dir changed

--- 90-script/pre-commit.sh
+++ 90-script/pre-commit.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
-echo "Running pre-commit hook"
+git diff --cached --name-only | if grep --quiet "03-code/"
+then
+  echo "Running pre-commit tests"
 
-# run tests
-cd 03-code || exit
-just test
+  # run tests
+  cd 03-code || exit
+  just test
+fi
 
 # $? stores exit value of the last command
 if [ $? -ne 0 ]; then
  echo "Tests must pass before commit"
  exit 1
 fi
+
