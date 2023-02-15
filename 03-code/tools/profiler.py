b30130ecc0c8afbf11e0557fee55f6606a849eb1
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2023-02-15 11:43:25 +0000

    🎨 tweak plotting code

--- 03-code/tools/profiler.py
+++ 03-code/tools/profiler.py
@@ -31,13 +31,13 @@ def read_stack_ptr_log_file(filepath: Path):
         return values
 
 
-def plot_max_value(ax, max_value):
+def plot_max_value(ax, max_value, ymax):
     max_value_colour = "#D65F5F"
     ax.axhline(max_value, linestyle="--", color=max_value_colour)
-    ax.text(0, max_value + 12, f"$\\max = {max_value}$", color=max_value_colour)
+    ax.text(0, max_value + ymax * 0.025, f"$\\max = {max_value}$", color=max_value_colour)
 
 
-def plot_stack_memory_usage(stack_ptr_log_file: Path, plot_output_file: Path | None, title: str):
+def plot_stack_memory_usage(stack_ptr_log_file: Path, plot_output_file: Path | None, title: str, show_plot: bool):
     log_values = read_stack_ptr_log_file(stack_ptr_log_file)
     max_value = np.max(log_values)
     x = np.arange(len(log_values))
@@ -51,24 +51,28 @@ def plot_stack_memory_usage(stack_ptr_log_file: Path, plot_output_file: Path | N
     if title:
         ax.set_title(title)
 
-    plot_max_value(ax, max_value)
+    ymax = max_value * 1.1
+    ax.set_ylim([0, ymax])
+
+    plot_max_value(ax, max_value, ymax)
 
     if plot_output_file is not None:
         plt.savefig(plot_output_file)
         print(f"Plot saved to {plot_output_file}")
 
-    plt.show()
+    if show_plot:
+        plt.show()
 
 
 def compare_stack_memory_usage(stack_ptr_log_file_1: Path, stack_ptr_log_file_2: Path, plot_output_file: Path | None,
-                               title: str, subtitle1: str, subtitle2: str):
+                               title: str, subtitle1: str, subtitle2: str, show_plot: bool):
     log_values_1 = read_stack_ptr_log_file(stack_ptr_log_file_1)
     log_values_2 = read_stack_ptr_log_file(stack_ptr_log_file_2)
 
     max_value_1 = np.max(log_values_1)
     max_value_2 = np.max(log_values_2)
 
-    fig, (ax1, ax2) = plt.subplots(1, 2, sharey="all", figsize=(8, 4))
+    fig, (ax1, ax2) = plt.subplots(1, 2, sharey="all", figsize=(10, 4))
 
     x1 = np.arange(len(log_values_1))
     x2 = np.arange(len(log_values_2))
@@ -81,7 +85,11 @@ def compare_stack_memory_usage(stack_ptr_log_file_1: Path, stack_ptr_log_file_2:
     if subtitle1:
         ax1.set_title(subtitle1)
 
-    plot_max_value(ax1, max_value_1)
+    # add some extra space at top of y axis, to allow for "max" text
+    ymax = max(max_value_1, max_value_2) * 1.1
+    ax1.set_ylim([0, ymax])
+
+    plot_max_value(ax1, max_value_1, ymax)
 
     ax2.bar(x2, log_values_2, width=1, rasterized=True)
     ax2.set_xlabel(r"Program execution $\rightarrow$")
@@ -90,7 +98,7 @@ def compare_stack_memory_usage(stack_ptr_log_file_1: Path, stack_ptr_log_file_2:
     if subtitle2:
         ax2.set_title(subtitle2)
 
-    plot_max_value(ax2, max_value_2)
+    plot_max_value(ax2, max_value_2, ymax)
 
     if title:
         fig.suptitle(title)
@@ -99,7 +107,8 @@ def compare_stack_memory_usage(stack_ptr_log_file_1: Path, stack_ptr_log_file_2:
         plt.savefig(plot_output_file, dpi=1200)
         print(f"Plot saved to {plot_output_file}")
 
-    plt.show()
+    if show_plot:
+        plt.show()
 
 
 def stack_memory_usage_profiler(args):
@@ -112,9 +121,9 @@ def stack_memory_usage_profiler(args):
     if args.compare:
         stack_ptr_log_file_2 = Path(args.logfile2).resolve()
         compare_stack_memory_usage(stack_ptr_log_file, stack_ptr_log_file_2, output_plot_path, args.title,
-                                   args.subtitle1, args.subtitle2)
+                                   args.subtitle1, args.subtitle2, not args.noshow)
     else:
-        plot_stack_memory_usage(stack_ptr_log_file, output_plot_path, args.title)
+        plot_stack_memory_usage(stack_ptr_log_file, output_plot_path, args.title, not args.noshow)
 
 
 if __name__ == "__main__":
@@ -131,6 +140,7 @@ if __name__ == "__main__":
     parser_stack.add_argument("--title", default="", help="Plot title")
     parser_stack.add_argument("--subtitle1", default="", help="Title for 1st subplot")
     parser_stack.add_argument("--subtitle2", default="", help="Title for 2nd subplot")
+    parser_stack.add_argument("--noshow", action="store_true", help="Don't open the plot window")
     # define the function to call if the stack subcommand is used
     parser_stack.set_defaults(func=stack_memory_usage_profiler)
 
